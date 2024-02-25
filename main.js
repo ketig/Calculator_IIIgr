80515995ed09ec1662bd58638375acbb0eac93d1
ketig <41895005+ketig@users.noreply.github.com>
2024-02-25 18:44:45 +0400

    functions

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-function addToDisplay() {
-    document.getElementById("display").value = "1234";
+function addToDisplay(value) {
+    document.getElementById("display").value += value;
+}
+function clearDisplay() {
+    clearInput("display");
+}
+function clearAll() {
+    clearDisplay();
+    clearInput("displayResult");
+}
+function clearInput(id) {
+    document.getElementById(id).value = "";
 }
